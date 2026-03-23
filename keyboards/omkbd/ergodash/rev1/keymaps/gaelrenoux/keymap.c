7395fccaba0a302eb166e37d4153069b8038cd9b
Gaël Renoux <gael.renoux@gmail.com>
2026-03-23 19:44:22 +0100

    fix indentation on base layer

--- keyboards/omkbd/ergodash/rev1/keymaps/gaelrenoux/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/gaelrenoux/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   à  |   y  |   x  |   .  |   k  |   _  |                    | C-Ent|   '  |   q  |   g  |   h  |   f  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Raise| Ctrl |  GUI |  Alt |||||||| Lower| Space|  Del |||||||| Bksp | Enterf| AltGr|||||||| Left | Down |  Up  | Right|
+   * | Raise| Ctrl |  GUI |  Alt |||||||| Lower| Space|  Del |||||||| Bksp | Enter| AltGr|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,                         KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RALT(KC_SPC),              LCTL(KC_ENT), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    RAISE,   KC_LCTL, KC_LGUI, KC_LALT,          LOWER,   KC_SPC, KC_DEL,        KC_BSPC, KC_ENT , KC_RALT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    RAISE,   KC_LCTL, KC_LGUI, KC_LALT,          LOWER,   KC_SPC,  KC_DEL,       KC_BSPC, KC_ENT,  KC_RALT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Lower: Symbols and numbers
