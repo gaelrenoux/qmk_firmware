164bd362ec439b001784199c2aa09f722f0de4ee
Gaël Renoux <gael.renoux@gmail.com>
2026-03-23 18:33:59 +0100

    Direct access to ctrl+enter and underscore

--- keyboards/omkbd/ergodash/rev1/keymaps/gaelrenoux/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/gaelrenoux/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   A  |   S  |   D  |   F  |   G  |  Del |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | ???? |                    | C-Ent|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | Raise| Ctrl |  GUI |  Alt |||||||| Lower| Space|  Del |||||||| Bksp | Enter| AltGr|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -42,17 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   a  |   u  |   i  |   e  |   ,  |  Del |                    |   w  |   c  |   t  |   s  |   r  |   n  |  m   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   à  |   y  |   x  |   .  |   k  | Space|                    | Enter|   '  |   q  |   g  |   h  |   f  | Shift|
+   * | Shift|   à  |   y  |   x  |   .  |   k  |   _  |                    | C-Ent|   '  |   q  |   g  |   h  |   f  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Raise| Ctrl |  GUI |  Alt |||||||| Lower| Space|  Del |||||||| Bksp | Enter| AltGr|||||||| Left | Down |  Up  | Right|
+   * | Raise| Ctrl |  GUI |  Alt |||||||| Lower| Space|  Del |||||||| Bksp | Enterf| AltGr|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR,                        KC_CAPS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    RAISE,   KC_LCTL, KC_LGUI, KC_LALT,          LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , KC_RALT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,                         KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RALT(KC_SPC),              LCTL(KC_ENT), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    RAISE,   KC_LCTL, KC_LGUI, KC_LALT,          LOWER,   KC_SPC, KC_DEL,        KC_BSPC, KC_ENT , KC_RALT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Lower: Symbols and numbers
